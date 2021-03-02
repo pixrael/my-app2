e2dda20c1d13a318079c83ca471de1389277cb3f
pixrael <pixrael777@gmail.com>
2021-03-02 18:11:04 -0300

    t2-created toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { MaterialModule } from '../app/material/material.module';
+
 import { AppComponent } from './app.component';
-
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from '@angular/material/slider';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MatSliderModule,
-    NoopAnimationsModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
